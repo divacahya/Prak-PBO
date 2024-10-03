a8b966075645c3785f7203b8666fcfb3f766c0d5
divacahya <spodiva@gmail.com>
2024-10-03 14:47:39 +0700

    Delete p pbo 3 directory

--- p pbo 3/Dosen.java
+++
@@ -1,6 +0,0 @@
-public class Dosen extends Karyawan {
-    
-    Dosen(String kodeKaryawan, String nama) {
-        super(kodeKaryawan, nama);
-    }
-}
--- p pbo 3/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Dosen buion = new Dosen("4444", "Bu Ion");
-        buion.getinfo();
-
-        Karyawan maswahyu = new Karyawan("3333", "Mas Wahyu");
-        maswahyu.getinfo();
-    }
-}
